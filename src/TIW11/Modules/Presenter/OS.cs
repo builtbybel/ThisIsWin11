8b599efd31f76f4f47ecdfbb542fa04a0f0ce33a
Belim <belim@builtbybel.com>
2021-11-01 10:02:13 +0100

    Add files via upload

---
+++ src/TIW11/Modules/Presenter/OS.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Win32;
+using System;
+
+namespace ThisIsWin11.Presenter
+{
+    public class OS
+    {
+        public string ComputerName { get; set; }
+
+        public bool IsWin11()
+        {
+            ComputerName = Environment.MachineName;
+
+            try
+            {
+                RegistryKey key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion");
+                int osbuild = Convert.ToInt32(key.GetValue("CurrentBuildNumber"));
+                if (osbuild >= 22000)
+                {
+                    return true;
+                }
+            }
+            catch { }
+            return false;
+        }
+
+        public string GetVersion()
+        {
+            RegistryKey key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion");
+
+            var UBR = key.GetValue("UBR").ToString();
+            var CurrentBuild = key.GetValue("CurrentBuild").ToString();
+
+            string version = CurrentBuild + "." + UBR;
+
+            return "Build " + version;
+        }
+
+        public string Is64Bit()
+        {
+            string bitness = string.Empty;
+
+            if (Environment.Is64BitOperatingSystem)
+            {
+                bitness = "64bit";
+            }
+            else
+            {
+                bitness = "32bit";
+            }
+
+            return bitness;
+        }
+    }
+}
