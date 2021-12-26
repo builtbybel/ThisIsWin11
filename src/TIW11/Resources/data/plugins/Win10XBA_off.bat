1737b60862dae904a568c8b567690a91c91efb05
Vichingo455 <59311016+Vichingo455@users.noreply.github.com>
2021-12-26 14:05:20 +0100

    Windows 10X Boot Animation Plugin

---
+++ src/TIW11/Resources/data/plugins/Win10XBA_off.bat
@@ -0,0 +1,2 @@
+@echo off
+reg delete "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\BootControl" /f
---
+++ src/TIW11/Resources/data/plugins/Win10XBA_on.bat
@@ -0,0 +1,2 @@
+@echo off
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\BootControl" /v "BootAnimation" /t REG_DWORD /d 1 /f
