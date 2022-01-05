8f3508cadcf735fd3126fb8bebb1fd7637686109
Vichingo455 <59311016+Vichingo455@users.noreply.github.com>
2022-01-05 16:18:48 +0100

    Set MainWindow.cs to Maximized part 1

--- src/TIW11/MainWindow.Designer.cs
+++ src/TIW11/MainWindow.Designer.cs
@@ -61,9 +61,10 @@ namespace ThisIsWin11
             this.pnlNav.Controls.Add(this.btnCustomize);
             this.pnlNav.Dock = System.Windows.Forms.DockStyle.Left;
             this.pnlNav.Location = new System.Drawing.Point(0, 0);
+            this.pnlNav.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pnlNav.Name = "pnlNav";
-            this.pnlNav.Padding = new System.Windows.Forms.Padding(0, 20, 0, 0);
-            this.pnlNav.Size = new System.Drawing.Size(38, 862);
+            this.pnlNav.Padding = new System.Windows.Forms.Padding(0, 25, 0, 0);
+            this.pnlNav.Size = new System.Drawing.Size(48, 1055);
             this.pnlNav.TabIndex = 130;
             // 
             // btnGlobalNav
@@ -76,10 +77,10 @@ namespace ThisIsWin11
             this.btnGlobalNav.Font = new System.Drawing.Font("Segoe Fluent Icons", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnGlobalNav.ForeColor = System.Drawing.Color.DimGray;
             this.btnGlobalNav.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.btnGlobalNav.Location = new System.Drawing.Point(8, 6);
-            this.btnGlobalNav.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnGlobalNav.Location = new System.Drawing.Point(10, 8);
+            this.btnGlobalNav.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnGlobalNav.Name = "btnGlobalNav";
-            this.btnGlobalNav.Size = new System.Drawing.Size(27, 32);
+            this.btnGlobalNav.Size = new System.Drawing.Size(34, 40);
             this.btnGlobalNav.TabIndex = 146;
             this.btnGlobalNav.TabStop = false;
             this.btnGlobalNav.Text = "...";
@@ -99,10 +100,10 @@ namespace ThisIsWin11
             this.btnExtensions.Font = new System.Drawing.Font("Segoe Fluent Icons", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnExtensions.ForeColor = System.Drawing.Color.DimGray;
             this.btnExtensions.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.btnExtensions.Location = new System.Drawing.Point(8, 242);
-            this.btnExtensions.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnExtensions.Location = new System.Drawing.Point(10, 302);
+            this.btnExtensions.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnExtensions.Name = "btnExtensions";
-            this.btnExtensions.Size = new System.Drawing.Size(24, 32);
+            this.btnExtensions.Size = new System.Drawing.Size(34, 40);
             this.btnExtensions.TabIndex = 145;
             this.btnExtensions.TabStop = false;
             this.btnExtensions.Text = "5";
@@ -122,10 +123,10 @@ namespace ThisIsWin11
             this.btnHome.Font = new System.Drawing.Font("Segoe Fluent Icons", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnHome.ForeColor = System.Drawing.Color.MediumVioletRed;
             this.btnHome.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.btnHome.Location = new System.Drawing.Point(8, 45);
-            this.btnHome.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnHome.Location = new System.Drawing.Point(10, 56);
+            this.btnHome.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnHome.Name = "btnHome";
-            this.btnHome.Size = new System.Drawing.Size(27, 32);
+            this.btnHome.Size = new System.Drawing.Size(34, 40);
             this.btnHome.TabIndex = 144;
             this.btnHome.Text = "0";
             this.btnHome.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -145,10 +146,10 @@ namespace ThisIsWin11
             this.btnSettings.Font = new System.Drawing.Font("Segoe Fluent Icons", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSettings.ForeColor = System.Drawing.Color.DimGray;
             this.btnSettings.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.btnSettings.Location = new System.Drawing.Point(8, 817);
-            this.btnSettings.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnSettings.Location = new System.Drawing.Point(10, 998);
+            this.btnSettings.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnSettings.Name = "btnSettings";
-            this.btnSettings.Size = new System.Drawing.Size(24, 32);
+            this.btnSettings.Size = new System.Drawing.Size(34, 40);
             this.btnSettings.TabIndex = 143;
             this.btnSettings.Text = "..";
             this.btnSettings.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -166,10 +167,10 @@ namespace ThisIsWin11
             this.btnAutomate.Font = new System.Drawing.Font("Segoe Fluent Icons", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAutomate.ForeColor = System.Drawing.Color.DimGray;
             this.btnAutomate.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.btnAutomate.Location = new System.Drawing.Point(8, 202);
-            this.btnAutomate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnAutomate.Location = new System.Drawing.Point(10, 252);
+            this.btnAutomate.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnAutomate.Name = "btnAutomate";
-            this.btnAutomate.Size = new System.Drawing.Size(24, 32);
+            this.btnAutomate.Size = new System.Drawing.Size(34, 40);
             this.btnAutomate.TabIndex = 142;
             this.btnAutomate.TabStop = false;
             this.btnAutomate.Text = "4";
@@ -189,10 +190,10 @@ namespace ThisIsWin11
             this.btnPackages.Font = new System.Drawing.Font("Segoe Fluent Icons", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnPackages.ForeColor = System.Drawing.Color.DimGray;
             this.btnPackages.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.btnPackages.Location = new System.Drawing.Point(8, 162);
-            this.btnPackages.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnPackages.Location = new System.Drawing.Point(10, 202);
+            this.btnPackages.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnPackages.Name = "btnPackages";
-            this.btnPackages.Size = new System.Drawing.Size(24, 32);
+            this.btnPackages.Size = new System.Drawing.Size(34, 40);
             this.btnPackages.TabIndex = 141;
             this.btnPackages.TabStop = false;
             this.btnPackages.Text = "3";
@@ -212,10 +213,10 @@ namespace ThisIsWin11
             this.btnApps.Font = new System.Drawing.Font("Segoe Fluent Icons", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnApps.ForeColor = System.Drawing.Color.DimGray;
             this.btnApps.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.btnApps.Location = new System.Drawing.Point(8, 122);
-            this.btnApps.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnApps.Location = new System.Drawing.Point(10, 152);
+            this.btnApps.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnApps.Name = "btnApps";
-            this.btnApps.Size = new System.Drawing.Size(24, 32);
+            this.btnApps.Size = new System.Drawing.Size(34, 40);
             this.btnApps.TabIndex = 140;
             this.btnApps.TabStop = false;
             this.btnApps.Text = "2";
@@ -235,10 +236,10 @@ namespace ThisIsWin11
             this.btnCustomize.Font = new System.Drawing.Font("Segoe Fluent Icons", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCustomize.ForeColor = System.Drawing.Color.DimGray;
             this.btnCustomize.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.btnCustomize.Location = new System.Drawing.Point(8, 82);
-            this.btnCustomize.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnCustomize.Location = new System.Drawing.Point(10, 102);
+            this.btnCustomize.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnCustomize.Name = "btnCustomize";
-            this.btnCustomize.Size = new System.Drawing.Size(24, 32);
+            this.btnCustomize.Size = new System.Drawing.Size(34, 40);
             this.btnCustomize.TabIndex = 139;
             this.btnCustomize.TabStop = false;
             this.btnCustomize.Text = "1";
@@ -252,25 +253,27 @@ namespace ThisIsWin11
             this.pnlContainer.AutoScroll = true;
             this.pnlContainer.BackColor = System.Drawing.Color.White;
             this.pnlContainer.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnlContainer.Location = new System.Drawing.Point(38, 0);
+            this.pnlContainer.Location = new System.Drawing.Point(48, 0);
+            this.pnlContainer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pnlContainer.Name = "pnlContainer";
-            this.pnlContainer.Size = new System.Drawing.Size(1200, 862);
+            this.pnlContainer.Size = new System.Drawing.Size(1500, 1055);
             this.pnlContainer.TabIndex = 136;
             // 
             // menuMain
             // 
             this.menuMain.BackColor = System.Drawing.Color.WhiteSmoke;
             this.menuMain.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuMain.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.menuMainComponents});
             this.menuMain.Name = "menuMain";
             this.menuMain.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
-            this.menuMain.Size = new System.Drawing.Size(174, 34);
+            this.menuMain.Size = new System.Drawing.Size(201, 40);
             // 
             // menuMainComponents
             // 
             this.menuMainComponents.Name = "menuMainComponents";
-            this.menuMainComponents.Size = new System.Drawing.Size(173, 30);
+            this.menuMainComponents.Size = new System.Drawing.Size(200, 36);
             this.menuMainComponents.Text = "Extensions";
             // 
             // tt
@@ -286,17 +289,19 @@ namespace ThisIsWin11
             // 
             // MainWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1238, 862);
+            this.ClientSize = new System.Drawing.Size(1548, 1055);
             this.Controls.Add(this.pnlContainer);
             this.Controls.Add(this.pnlNav);
             this.Font = new System.Drawing.Font("Segoe UI Variable Display", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.MinimumSize = new System.Drawing.Size(1182, 809);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MinimumSize = new System.Drawing.Size(1473, 999);
             this.Name = "MainWindow";
             this.ShowIcon = false;
             this.Text = "ThisIsWin11";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Shown += new System.EventHandler(this.MainWindow_Shown);
             this.pnlNav.ResumeLayout(false);
             this.pnlNav.PerformLayout();
