0229330d56cfadd4677ffa5c503b5d7a772aa65f
Belim <belim@builtbybel.com>
2022-01-14 10:16:26 +0100

    Delete (Apps) Reinstall all built-in apps.ps1

--- src/TIW11/Resources/data/scripts/(Apps) Reinstall all built-in apps.ps1
+++
@@ -1,3 +0,0 @@
-###This script will reinstall all built-in modern apps in a single step. ###
-###This command will take a few moments and will reinstall all built-in apps. If you receive any error message, ignore it. You may need to restart the computer to take effect.
-Get-AppxPackage -allusers | foreach {Add-AppxPackage -register "$($_.InstallLocation)\appxmanifest.xml" -DisableDevelopmentMode}
