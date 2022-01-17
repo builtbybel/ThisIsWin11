81c7c712996528ffb2670697d1027adc25a5a694
Belim <belim@builtbybel.com>
2022-01-17 17:40:32 +0100

    Rollout 1.1.101

--- src/TIW11/Properties/AssemblyInfo.cs
+++ src/TIW11/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // indem Sie "*" wie unten gezeigt eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.20")]
-[assembly: AssemblyFileVersion("1.1.20")]
+[assembly: AssemblyVersion("1.1.101")]
+[assembly: AssemblyFileVersion("1.1.101")]
