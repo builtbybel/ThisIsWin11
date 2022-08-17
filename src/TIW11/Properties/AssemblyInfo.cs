7f728cf9380873b916f99e492768d50949ed3d7b
Belim <belim@builtbybel.com>
2022-08-17 17:13:51 +0200

    Push release 1.3.0.........

--- src/TIW11/Properties/AssemblyInfo.cs
+++ src/TIW11/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // indem Sie "*" wie unten gezeigt eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.220")]
-[assembly: AssemblyFileVersion("1.2.220")]
+[assembly: AssemblyVersion("1.3.0")]
+[assembly: AssemblyFileVersion("1.3.0")]
